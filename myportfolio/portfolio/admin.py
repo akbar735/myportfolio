36bfe778352bf51aa2b6d9b7ca4037b9bcd91644
akbar735 <mdaliakbar858@gmail.com>
2025-04-14 16:30:10 +0530

    first commit

---
+++ myportfolio/portfolio/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Portfolio, Skill, Project, TechStack, InterestedArea, Achievement, Hobbies, Education
+# Register your models here.
+
+admin.site.register(Portfolio)
+admin.site.register(Skill)
+admin.site.register(Project)
+admin.site.register(TechStack)
+admin.site.register(InterestedArea)
+admin.site.register(Achievement)
+admin.site.register(Hobbies)
+admin.site.register(Education)
---
+++ myportfolio/portfolio/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+
+class ExpertiseLevel(models.TextChoices):
+    BEGINNER = 'BEGINNER', 'Beginner'
+    INTERMEDIATE = 'INTERMEDIATE', 'Intermediate'
+    ADVANCED = 'ADVANCED', 'Advanced'
+    EXPERT = 'EXPERT', 'Expert'
+
+# Create your models here.
+
+    
+    
+class Portfolio(models.Model):
+    title = models.CharField(max_length=50)
+    intro = models.CharField(max_length=1000)
+    resume = models.FileField(upload_to='resumes/', null=True, blank=True)
+    mobile = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+    linked_in = models.CharField(max_length=30)
+    github = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+    
+class Skill(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='skills')
+    title = models.CharField(max_length=50)
+    experties = models.CharField(max_length=20, choices=ExpertiseLevel.choices, default=ExpertiseLevel.BEGINNER)
+
+    def __str__(self):
+       return f"{self.title} ({self.get_expertise_display()})"
+    
+class Project(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='projects')
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    start_date = models.DateField()
+    end_date = models.CharField(max_length=50)
+    client_name = models.CharField(max_length=50, null=True, blank=True)
+    
+    def __str__(self):
+       return f"{self.title}"
+
+class TechStack(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True, related_name='techstack')
+    name = models.CharField(max_length=50)
+    def __str__(self):
+       return f"{self.name}"
+
+class InterestedArea(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='interested_areas', null=True, blank=True)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+       return f"{self.title}"
+    
+
+class Achievement(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='achievements', null=True, blank=True)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+       return f"{self.title}"
+    
+class Hobbies(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='hobbies', null=True, blank=True)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+       return f"{self.title}"
+
+class Education(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='educations')
+    course = models.CharField(max_length=50)
+    intitution = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    score = models.CharField(max_length=10)
+    
+    def __str__(self):
+       return f"{self.course}" 
